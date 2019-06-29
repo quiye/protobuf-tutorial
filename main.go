daf333ab741cf074175b671444bac53872a0bf3e
quiye <zamasu@gmail.com>
2019-06-29 04:59:20 +0000

    httptest

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("debug %v\n", r)
+	fmt.Fprintf(w, "Hello, World\n")
+}
+
 func main() {
-	fmt.Println("Hello, playground")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
 
 func one() int {
--- main_test.go
+++ main_test.go
@@ -1,6 +1,19 @@
 package main
 
-import "testing"
+import (
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	req := httptest.NewRequest("GET", "/hello", nil)
+	rec := httptest.NewRecorder()
+	handler(rec, req)
+
+	if rec.Body.String() != "Hello, World\n" {
+		t.Error()
+	}
+}
 
 func TestOne(t *testing.T) {
 	r := one()
